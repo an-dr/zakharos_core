ec8e9360fbd832f52d75035a973faad22145ce78
Andrei Gramakov <mail@agramakov.me>
2021-01-07 00:29:56 +0100

    Add tools for interacting with devices without ROS

---
+++ tools/i2c/moving_platform_commander.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+from zakhar_pycore.i2c import write
+from zakhar_pycore.constants import ADDR
+from zakhar_pycore.helpers.os import wait_key
+
+if __name__ == "__main__":
+    while (1):
+        print("Press the key (Esc to exit): ")
+        key = wait_key()
+        if key == 0x1b:
+            print("Exit")
+            exit()
+        write(ADDR.I2C.MOTORS, 0, key)  # write
