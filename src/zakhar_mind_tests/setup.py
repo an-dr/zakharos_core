7248f68a5247bc8f87226e2ce3af49a214b40597
Andrei Gramakov <mail@agramakov.me>
2021-01-07 00:42:57 +0100

    Add a package for testing interaction from ego to hardware

--- src/zakhar_mind_tests/setup.py
+++ src/zakhar_mind_tests/setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 from catkin_pkg.python_setup import generate_distutils_setup
 
 d = generate_distutils_setup(
-    packages=['zakhar_i2c_devices'],
+    packages=['zakhar_mind_tests'],
     package_dir={'': 'src'},
 )
 
