29252f6dea070963ed26dddc3bb5ac800d7fb8fe
Andrei Gramakov <mail@agramakov.me>
2021-01-16 12:39:39 +0000

    wip

--- src/zakhar_emotion_core/src/zakhar_emotion_core_node.cpp
+++ src/zakhar_emotion_core/src/zakhar_emotion_core_node.cpp
@@ -12,26 +12,43 @@
 
 #include "ros/ros.h"
 #include "zakhar_msgs/EmotionCoreDataDescriptor.h"
+#include "zakhar_msgs/EmotionCoreWrite.h"
+#include "zakhar_msgs/EmotionParams.h"
 
-bool add(zakhar_msgs::EmotionCoreDataDescriptor::Request  &req,
-         zakhar_msgs::EmotionCoreDataDescriptor::Response &res )
-{
-//   res.sum = req.a + req.b;
-//   ROS_INFO("request: x=%ld, y=%ld", (long int)req.a, (long int)req.b);
-//   ROS_INFO("  sending back response: [%ld]", (long int)res.sum);
-  return true;
+
+bool EmotionCoreWrite_handler(zakhar_msgs::EmotionCoreWrite::Request &req,
+                              zakhar_msgs::EmotionCoreWrite::Response &res) {
+    res.result = "done";
+    return true;
 }
 
-int main(int argc, char **argv)
-{
-  ros::init(argc, argv, "zakhar_emotion_core");
-  ros::NodeHandle n;
+bool EmotionCoreDataDescriptor_handler(zakhar_msgs::EmotionCoreDataDescriptor::Request &req,
+                                       zakhar_msgs::EmotionCoreDataDescriptor::Response &res) {
+    res.result = "done";
+    return true;
+}
 
-// %Tag(SERVICE_SERVER)%
-  ros::ServiceServer service = n.advertiseService("add_two_ints", add);
-// %EndTag(SERVICE_SERVER)%
+bool FillMsg(zakhar_msgs::EmotionParams &msg){
+    msg.params_json = "{}";
+    return true;
+}
 
-  ros::spin();
 
-  return 0;
+int main(int argc, char **argv) {
+    ros::init(argc, argv, "zakhar_emotion_core");
+    ros::NodeHandle n;
+
+    ros::ServiceServer emotion_core_write_srv           = n.advertiseService("EmotionCoreWrite", EmotionCoreWrite_handler);
+    ros::ServiceServer emotion_core_data_descriptor_srv = n.advertiseService("EmotionCoreDataDescriptor.", EmotionCoreDataDescriptor_handler);
+    ros::Publisher emotion_core_emotion_params_pub      = n.advertise<zakhar_msgs::EmotionParams>("/EmotionParams", 10);
+    ros::Rate loop_rate(10);
+    while (ros::ok()) {
+        zakhar_msgs::EmotionParams msg;
+        FillMsg(msg);
+        ROS_INFO("%s", msg.params_json);
+        emotion_core_emotion_params_pub.publish(msg);
+        ros::spinOnce();
+        loop_rate.sleep();
+    }
+    return -1;
 }
