73197a6b6e6ae40230d99c60bfb39c7e614854ba
Andrei Gramakov <mail@agramakov.me>
2021-01-22 22:20:06 +0000

    Rename zakhar_mind_tests package to zakhar_tests

--- src/zakhar_tests/nodes/test_ego_face.py
+++ src/zakhar_tests/nodes/test_ego_face.py
@@ -3,21 +3,21 @@ import rospy
 from zakhar_mind.ego_like import EgoLikeNode
 from time import sleep
 
-class EgoMotorsTest(EgoLikeNode):
-    def __init__(self, name="EgoMotorsTest"):
+
+class EgoFaceTest(EgoLikeNode):
+    def __init__(self, name="EgoFaceTest"):
         EgoLikeNode.__init__(self, name=name)
 
     def main(self):
         while (1):
-            self.to_will("move_forward")
+            self.to_will("hello")
             sleep(1)
-            self.to_will("move_shiver")
-            sleep(2)
-            self.to_will("move_stop")
+            self.to_will("bye")
             sleep(1)
 
+
+ego = EgoFaceTest()
 
 if __name__ == '__main__':
-    ego = EgoMotorsTest()
     ego.start()
     rospy.spin()
--- src/zakhar_tests/nodes/test_ego_motors.py
+++ src/zakhar_tests/nodes/test_ego_motors.py
@@ -3,19 +3,22 @@ import rospy
 from zakhar_mind.ego_like import EgoLikeNode
 from time import sleep
 
-class EgoFaceTest(EgoLikeNode):
-    def __init__(self, name="EgoFaceTest"):
+
+class EgoMotorsTest(EgoLikeNode):
+    def __init__(self, name="EgoMotorsTest"):
         EgoLikeNode.__init__(self, name=name)
 
     def main(self):
         while (1):
-            self.to_will("hello")
+            self.to_will("move_forward")
             sleep(1)
-            self.to_will("bye")
+            self.to_will("move_shiver")
+            sleep(2)
+            self.to_will("move_stop")
             sleep(1)
 
-ego = EgoFaceTest()
 
 if __name__ == '__main__':
+    ego = EgoMotorsTest()
     ego.start()
     rospy.spin()
