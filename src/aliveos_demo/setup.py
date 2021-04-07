880dd7ebd0811026062e357144b1983f13f6b368
an-dr <mail@agramakov.me>
2021-04-07 21:09:55 +0200

    Rename `aliveos_test` package to `aliveos_demo`

--- src/aliveos_demo/setup.py
+++ src/aliveos_demo/setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 from catkin_pkg.python_setup import generate_distutils_setup
 
 d = generate_distutils_setup(
-    packages=['aliveos_test'],
+    packages=['aliveos_demo'],
     package_dir={'': 'src'},
 )
 
