e32547b5ddccaede533f59a553452baf317bb757
an-dr <mail@agramakov.me>
2021-04-11 16:36:56 +0200

    Replace AliveOS packages with the metapackage v 0.1.1

--- src/aliveos_middle/setup.py
+++
@@ -1,9 +0,0 @@
-from distutils.core import setup
-from catkin_pkg.python_setup import generate_distutils_setup
-
-d = generate_distutils_setup(
-    packages=['aliveos_middle'],
-    package_dir={'': 'py'},
-)
-
-setup(**d)
